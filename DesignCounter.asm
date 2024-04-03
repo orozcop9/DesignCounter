a5e8ee4b2fbb7fe2bb3105a939da74a2d2f26880
orozcop9 <133064795+orozcop9@users.noreply.github.com>
2024-04-03 10:07:12 -0700

    Update DesignCounter.asm

--- DesignCounter.asm
+++ DesignCounter.asm
@@ -1,229 +1,174 @@
 ;---------------------
 ; Title: Counter Design
 ;---------------------
-;Program Details:
-;The purpose of this program is to have a 7 segment turn on and increment when in our case 
-;switch A is pressed and stop incrementing once it is released. It also needs to decrement
-;the same way but pressing switch B and stop once it is released, if both switches are
-;pressed then the counter resets to zero. the range in which this counter goes to is 
-;0-F
+; Program Details:
+; The purpose of this program is to have a 7-segment display turn on and increment when switch A is pressed and stop incrementing once it is released.
+; It also needs to decrement the same way when switch B is pressed and stop once it is released.
+; If both switches are pressed simultaneously, the counter resets to zero.
+; The range of the counter is from 0 to F.
 ; Outputs: RD0 (output)
-; inputs:  RC0 and RC1 (inputs)
+; Inputs: RC0 and RC1 (inputs)
 ; Setup: The PICKIT4 can use its internal 5V. No external power is needed.
-; Date: march 27, 2024
-; File Dependencies / Libraries: It is required to include the 
-;   AssemblyConfig.inc in the Header Folder
+; Date: March 27, 2024
+; File Dependencies / Libraries: It is required to include the AssemblyConfig.inc in the Header Folder
 ; Compiler: xc8, 2.4
 ; Author: Pablo Orozco
 ; Versions:
 ; V1.0: March 23, 2024
 ; V2: March 26, 2024
+; V3: March 28, 2024 (Modified for the desired functionality)
+; V4: March 28, 2024 (Revised to address issues)
 ; Useful links: 
 ;       Datasheet: https://ww1.microchip.com/downloads/en/DeviceDoc/PIC18(L)F26-27-45-46-47-55-56-57K42-Data-Sheet-40001919G.pdf 
 ;       PIC18F Instruction Sets: https://onlinelibrary.wiley.com/doi/pdf/10.1002/9781119448457.app4 
-;       List of Instrcutions: http://143.110.227.210/faridfarahmand/sonoma/courses/es310/resources/20140217124422790.pdf 
-
-;---------------------
-; Initialization
-;---------------------
-#include "AssemblyConfiguration.inc"
-#include <xc.inc>
-
-;---------------------
-; Program Inputs
-;---------------------
-LOOP1  EQU 0XFF // in decimal
-LOOP2  EQU 0XFF
-LOOP3  EQU 4
-
-;---------------------
-; Program Constants
-;---------------------
-REG10   EQU     10h   // in HEX
-REG11   EQU     11h
-REG12	EQU	12h
-BUFFER  EQU	3h
-BUFFER2	EQU	2h
+;       List of Instructions: http://143.110.227.210/faridfarahmand/sonoma/courses/es310/resources/20140217124422790.pdf 
 
 
+#include "AssemblyConfig.inc"
+#include "xc.inc"
 
+; Define delay constants
+INNER_LOOP       EQU 0xFF
+INNER_LOOP_REG   EQU 0x20
+INNER_LOOP_2     EQU 0xFF
+INNER_LOOP_2_REG EQU 0x24
+    
+; Constants
+#define BUTTON_1  PORTC, 0
+#define BUTTON_2  PORTC, 1
 
-;---------------------
-; Definitions
-;---------------------
-#define SWITCH_A    PORTC,0	; Switch A connected to RC0
-#define SWITCH_B    PORTC,1	; Switch B connected to RC1
-#define DISPLAY     PORTD	; 7-segment display connected to PORTC
-
-;---------------------
 ; Main Program
-;---------------------
-    PSECT absdata,abs,ovrld     ; Do not change
+PSECT absdata,abs,ovrld
 
-    ORG	    0         ;Reset vector
-    GOTO    _START1	
-    ORG 0020H
+    ORG 0x00           ; Reset vector
+    GOTO InitializePorts
 
-; Initialize the program
-_START1: 
+    ORG 0x0020         ; Begin assembly at 0020H
+    
+InitializePorts:
+    ; Initialize ports for inputs/outputs
+    BANKSEL PORTD
+    CLRF PORTD
+    BANKSEL LATD
+    CLRF LATD
+    BANKSEL ANSELD
+    CLRF ANSELD
+    BANKSEL TRISD
+    CLRF TRISD ; Set PORTD to outputs
+   
+    BANKSEL PORTC
+    CLRF PORTC
+    BANKSEL LATC
+    CLRF LATC
+    BANKSEL ANSELC
+    CLRF ANSELC
+    BANKSEL TRISC
+    MOVLW 0b00000011 ; Set RC0 & RC1 as inputs
+    MOVWF TRISC  
 
+    CALL SetupRegisters
+    CALL CheckReset
+    
+ButtonSetup:
+    BTFSS BUTTON_2 ; If not pressed, skip
+    GOTO Button2Pressed
+    BTFSS BUTTON_1 ; If not pressed, skip
+    GOTO Button1Pressed
+    GOTO ButtonSetup ; Constantly checking if a button is pushed
 
-    BANKSEL	PORTD 
-    CLRF	PORTD	;Init PORTD
-    BANKSEL	PORTC
-    CLRF	PORTC
-    BANKSEL	LATD	;Data Latch
-    CLRF	LATD ;
-    BANKSEL	ANSELD 
-    CLRF	ANSELD	;digital I/O
-    BANKSEL	LATC
-    CLRF	LATC
-    BANKSEL	ANSELC 
-    CLRF	ANSELC
-;	    
-    MOVLW	0b00000011 ;Set RD0 AND RD1 as inputs
-    MOVWF	TRISC
-
-    MOVLW	0b00000000; Set RD as outputs
-    MOVWF	TRISD
-
-	;indirect addressing to find the program memory    
-    MOVLW	0x0
-    MOVWF	TBLPTRL
-    MOVLW	0x02
-    TBLRD*	; reads value in 0x200 into latch
-
-MAIN:
-    CLRF    WREG
-    ; check if switch A is pressed(increment)
-    BTFSC   SWITCH_A
-    MOVLW   0x1
-    MOVFF   WREG, BUFFER
-    ; check if switch B is pressed(decrement)
-    BTFSC   SWITCH_B
-    MOVLW   0x2
-    MOVFF   WREG, BUFFER
-    ; check if both switches A and B are pressed (restart to zero)
-    BTFSS   SWITCH_A ; if both on
-    BRA	    0x6A     ; if set jump
-    BTFSS   SWITCH_B
-    BRA	    0x6A     ; if set jump
-    MOVLW   0x3
-    MOVFF   WREG, BUFFER
-
-    BTFSC   SWITCH_A ; if both off
-    BRA	    0x7A
-    BTFSC   SWITCH_B
-    BRA	    0x7A
-    MOVLW   0x4
-    MOVFF   WREG, BUFFER
-    GOTO    _BUFFCHECK
-
-_BUFFCHECK:
-    ;SETF 0x3FD8
-    MOVFF   BUFFER, BUFFER2
-    MOVLW   0x3
-    SUBWF   BUFFER2 ;set zero (3-2=1)
-    BZ	    MAIN
-    MOVFF   BUFFER, BUFFER2
-    MOVLW   0x4
-    SUBWF   BUFFER2 ;if equal
-    BZ	    _ZERO   ;goto to beginning
-    MOVFF   BUFFER, BUFFER2
-    MOVLW   0x1
-    SUBWF   BUFFER2
-    BZ	    COUNT_UP	;increment
-    MOVFF   BUFFER, BUFFER2
-    MOVLW   0x2
-    SUBWF   BUFFER2
-    BZ	    COUNT_DOWN	;decrement
-    MOVFF   BUFFER, BUFFER2
-
-_DISPLAY:
-    ; display on 7 segment
-    MOVFF   TABLAT, WREG    ;output of 7 segment
-    MOVFF   WREG, DISPLAY
-    CLRF    BUFFER
-    CLRF    BUFFER2
-    GOTO    MAIN
-
-_ZERO:
-    ;displays zero
-    MOVLW   0x0
-    MOVWF   TBLPTRL
-    MOVLW   0x02
-    MOVWF   TBLPTRH ;pointer looks at 0x200
-    TBLRD*
-    GOTO    _DISPLAY
-
-COUNT_UP:
-    ; incrementation when switch A is pressed
-    MOVLW    0x211
-    CPFSEQ  TBLPTR
-    BRA	    CONTINUE_COUNTUP
-    BRA	    RESTART_COUNTUP
-
-    CONTINUE_COUNTUP:
-	TBLRD+*	;increment 0x200 to 0x201 then copy value
-	MOVFF   TABLAT, WREG	
-	MOVFF   WREG, DISPLAY	;display output
-	call    _DELAY
-	GOTO    _DISPLAY
-
-
-COUNT_DOWN:
-    ; decrementation when switch b is pressed
-    MOVLW   0x1FF
-    CPFSEQ  TBLPTR
-    BRA	    CONTINUE_COUNTDOWN
-    BRA	    RESTART_COUNTDOWN
-
-    CONTINUE_COUNTDOWN:
-	TBLRD*-	; copy first then decrement
-	MOVFF   TABLAT, WREG
-	MOVFF   WREG, DISPLAY	; display output
-	call    _DELAY
-	GOTO    _DISPLAY
-
-RESTART_COUNTDOWN:
-    ;reset F to 0
-    MOVLW   0x10
-    MOVWF   TBLPTRL
-    MOVLW   0x02
-    MOVWF   TBLPTRH
-    TBLRD*
-    GOTO    _DISPLAY
-
-RESTART_COUNTUP:
-    MOVLW   0x0
-    MOVWF   TBLPTRL
-    MOVLW   0x02
-    MOVWF   TBLPTRH
-    TBLRD*
-    GOTO    _DISPLAY
-
-_DELAY:
-    MOVLW   LOOP1
-    MOVWF   REG10
-    MOVLW   LOOP2
-    MOVWF   REG11
-    MOVLW   LOOP3
-    MOVWF   REG12
-
-_LOOP:
-    DECF    REG10,1
-    BNZ	    _LOOP
-    DECF    REG11,1
-    BNZ	    _LOOP
-    DECF    REG12,1
-    BNZ	    _LOOP
+SetupRegisters:
+    MOVLW 0x00 ; Setting the upper table pointer
+    MOVWF TBLPTRU
+    MOVLW 0x01 ; Setting the higher table pointer
+    MOVWF TBLPTRH
     RETURN
 
+CheckReset:
+    MOVLW 0x60 ; Setting lower table pointer
+    MOVWF TBLPTRL
+    TBLRD*      ; Read from table latch
+    MOVFF TABLAT, PORTD ; Displaying 0
+    RCALL Delay   ; Rough 1 sec delay
+    RETURN
 
+Button2Pressed:
+    BTFSS BUTTON_1 ; Checks if both buttons are pressed
+    GOTO CheckReset ; If button 1 now pressed too, go to CheckReset
+    RCALL DisplayValue
+    RCALL CheckTableDecrement
+    RCALL Delay   ; Roughly 1 sec delay
+    DECF TBLPTRL, F ; Decrements from table pointer of seg table
+    GOTO Button2Pressed ; Loop decrementation
 
-    ORG 0x200
+CheckTableDecrement:
+    MOVLW 0x5F ; Pointed at before our seg table    
+    CPFSGT TBLPTRL ; If pointer is still within boundaries, skip    
+    GOTO ResetNegativeRegs ; Pointer is outside boundaries, reset
+    BTFSC BUTTON_2 ; Checks if button_2 is still pressed 
+    GOTO ButtonSetup ; If not pressed, go to button setup to hold value 
+    RETURN ; Return to decrementation loop
 
-    DB 0xC0, 0xC0, 0xF9, 0xA4, 0xB0
-    DB 0x99, 0x92, 0x82, 0xF8, 0X80
-    DB 0x90, 0x08, 0x03, 0x46, 0x21, 0x06, 0x0E, 0x0E
-    END
+ResetNegativeRegs:
+    MOVLW 0x00 ; Setting the upper table pointer
+    MOVWF TBLPTRU
+    MOVLW 0x01 ; Setting the higher table pointer
+    MOVWF TBLPTRH
+    MOVLW 0x6F ; Setting the higher table pointer
+    MOVWF TBLPTRL
+    RETURN
+
+Button1Pressed:
+    BTFSS BUTTON_2 ; Checks if both are pressed
+    GOTO CheckReset ; If now button_2 is pushed too, go to reset
+    RCALL DisplayValue ; Function to display value 
+    RCALL CheckTableIncrement ; Checks seg table pointers
+    RCALL Delay   ; Calls roughly 1 min delay
+    INCF TBLPTRL, F ; Increments table pointers to be ahead of display
+    GOTO Button1Pressed ; Loop incrementation
+
+DisplayValue:
+    TBLRD*
+    MOVFF TABLAT, PORTD
+    RETURN
+    
+CheckTableIncrement:
+    MOVLW 0x70 ; Puts value that is last on the seg table in WREG    
+    CPFSLT TBLPTRL ; Compares WREG to our lower table pointer (ahead)    
+    GOTO ResetPositiveRegs ; If out of bounds, resets table pointers 
+    BTFSC BUTTON_1 ; If in bounds, rechecks button pressed
+    GOTO ButtonSetup ; If button_1 is pressed, value is held and buttons are rechecked
+    RETURN ; Returns to incrementation loop
+    
+ResetPositiveRegs:
+    MOVLW 0x00 ; Setting the upper table pointer
+    MOVWF TBLPTRU
+    MOVLW 0x01 ; Setting the higher table pointer
+    MOVWF TBLPTRH
+    MOVLW 0x60 ; Setting the higher table pointer
+    MOVWF TBLPTRL
+    RETURN
+
+Delay:
+    MOVLW INNER_LOOP_2
+    MOVWF INNER_LOOP_2_REG
+    
+OuterLoop:  
+    MOVLW INNER_LOOP
+    MOVWF INNER_LOOP_REG
+    
+InnerLoop:
+    DECF INNER_LOOP_REG,F
+    NOP
+    NOP
+    BNZ InnerLoop
+    DECF INNER_LOOP_2_REG
+    BZ EndOfLoop
+    GOTO OuterLoop
+EndOfLoop:
+    RETURN
+    
+ORG 0x160
+SEG_TABLE:   DB  0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92 ;0-5 in display
+   DB  0x82, 0xF8, 0x80, 0x90,0x88 ; 6-10 in display
+   DB  0x83, 0xC6, 0xA1, 0x86 ; B-E in display
+   DB 0x8E; F (15) in display
